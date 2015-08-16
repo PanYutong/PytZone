9fe4c18b3b050b8b749ab4aae1e90decc03361a7
pyt <northernpyt@126.com>
2015-08-16 15:04:42 +0800

    small change.

--- Apactest/2015_08_16_test/test.cpp
+++ Apactest/2015_08_16_test/test.cpp
@@ -5,14 +5,15 @@
 #include <cstdio>
 #include <vector>
 #include <map>
-
+#include <cmath>
 using namespace std;
 
 int main (int argc, char * argv[]) {
 	int case_num = 0;
+	scanf("%d\n", &case_num);
 	for (int i = 0; i < case_num; i++) {
-		int No = i + 1;
-		printf("Case #%d: ", No);
+		No = case_num + 1;
+		printf("Case #%d: \n", No);
 	}
 	return 0;
 }
