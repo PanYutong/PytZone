60051c24153677aa0113cfb485ee5cfd2d88d8b4
pyt <northernpyt@126.com>
2015-08-16 12:31:29 +0800

    revise test.cpp

--- Apactest/2015_08_16_test/test.cpp
+++ Apactest/2015_08_16_test/test.cpp
@@ -9,5 +9,10 @@
 using namespace std;
 
 int main (int argc, char * argv[]) {
+	int case_num = 0;
+	for (int i = 0; i < case_num; i++) {
+		int No = i + 1;
+		printf("Case #%d: ", No);
+	}
 	return 0;
 }
