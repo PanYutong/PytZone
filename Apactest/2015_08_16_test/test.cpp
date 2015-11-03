693b184fe5219e09bae8c96028738fcafd4feffb
PanYutong <northernpyt@126.com>
2015-11-03 12:53:10 +0800

    template changed.

--- Apactest/2015_08_16_test/test.cpp
+++ Apactest/2015_08_16_test/test.cpp
@@ -8,12 +8,14 @@
 #include <cmath>
 using namespace std;
 
+#define inf (1000000000000LL + 7)
+#define ll long long
+
 int main (int argc, char * argv[]) {
 	int case_num = 0;
 	scanf("%d\n", &case_num);
-	for (int i = 0; i < case_num; i++) {
-		No = case_num + 1;
-		printf("Case #%d: \n", No);
+	for (int i = 1; i <= case_num; i++) {
+		printf("Case #%d: \n", i);
 	}
 	return 0;
 }
