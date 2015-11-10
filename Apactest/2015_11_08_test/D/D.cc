c4945dc4727bb8b6a6af27bb7cbd280001940c6f
PanYutong <northernpyt@126.com>
2015-11-10 13:15:06 +0800

    Apactest 2015_11_08 appanded.

---
+++ Apactest/2015_11_08_test/D/D.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const long long inf = 1LL << 62;
+
+typedef long long ll;
+
+int main (int argc, char * argv[]) {
+  int case_num = 0;
+  scanf("%d", &case_num);
+  for (int i = 1; i <= case_num; i++) {
+    printf("Case #%d: \n", i);
+  }
+  return 0;
+}
