95ed8fdf57fda98d818eaece5c817cf57f407baf
PanYutong <northernpyt@126.com>
2015-11-14 19:51:35 +0800

    Quick Sort Appended.

--- Apactest/2015_08_16_test/test.cc
+++ Apactest/2015_08_16_test/test.cc
@@ -8,13 +8,10 @@ const int intinf = 1 << 30;
 typedef long long ll;
 
 int main (int argc, char * argv[]) {
-  /*
   int case_num = 0;
   scanf("%d", &case_num);
   for (int i = 1; i <= case_num; i++) {
     printf("Case #%d: \n", i);
   }
-  */
-  cout << intinf << endl;
   return 0;
 }
