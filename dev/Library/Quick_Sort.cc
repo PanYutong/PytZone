865ad279809fc5390632597749ef295c0077dd02
PanYutong <northernpyt@126.com>
2015-11-14 19:53:55 +0800

    Quick Sort.

---
+++ dev/Library/Quick_Sort.cc
@@ -0,0 +1,48 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+
+vector<int> arr;
+
+
+int Partition(vector<int> &arr, int low, int high) {
+  arr[0] = arr[low];
+  int pivot = low;
+  while (low < high) {
+    while (arr[0] < arr[high] && high > low) --high;
+    arr[pivot] = arr[high];
+    pivot = high;
+    while (arr[0] > arr[low] && high > low) ++low;
+    arr[pivot] = arr[low];
+    pivot = low;
+  }
+  arr[pivot] = arr[0];
+  return pivot;
+}
+
+
+void QSort(vector<int> &arr, int low, int high) {
+  if (low < high) {
+    int pivot = Partition(arr, low, high);
+    QSort(arr, low, pivot - 1);
+    QSort(arr, pivot + 1, high);
+  }
+}
+
+
+int main() {
+  int N;
+  cin >> N;
+  arr.push_back(0);
+  for (int i = 0; i < N; ++i) {
+    int elem; cin >> elem;
+    arr.push_back(elem);
+  }
+  int low = 1, high = N;
+  QSort(arr, low, high);
+  for (int i = 1; i <= N; i++) {
+    cout << arr[i] << ' ';
+  }
+  cout << endl;
+  return 0;
+}
