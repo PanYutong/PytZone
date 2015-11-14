131b14ba9d900f8ef7c9c1b9bd1437d54e02f84b
PanYutong <northernpyt@126.com>
2015-11-14 17:28:00 +0800

    Prim alg appended.

--- dev/Library/Merge_Sort.cc
+++ dev/Library/Merge_Sort.cc
@@ -1,35 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> TMR;
+vector<int> t_ordered;
 void init(vector<int> &data) {
   int elem;
   data.push_back(0);
   while (cin >> elem) data.push_back(elem);
 }
 
-void Merge(vector<int> SP, vector<int> &MR, int s, int m, int t) {
-  int j, k;
-  for (k = s, j = m + 1; s <= m && j <= t; ++k) {
-    if (SP[s] <= SP[j]) MR[k] = SP[s++];
-    else MR[k] = SP[j++];
+void Merge(vector<int> origin, vector<int> &temp, int l, int m, int u) {
+  int k, j;
+  for (k = l, j = m + 1; l <= m && j <= u; ++k) {
+    if (origin[l] <= origin[j]) temp[k] = temp[l++];
+    else temp[k] = origin[j++];
   }
-  if (s <= m) {
-    for (int i = s; i <= m; i++, k++) MR[k] = SP[i];
+  if (l <= m) {
+    for (int i = l; i <= m; i++, k++) temp[k] = origin[i];
   }
-  if (j <= t) {
-    for (int i = j; i <= t; i++, k++) MR[k] = SP[i];
+  if (j <= u) {
+    for (int i = j; i <= u; i++, k++) temp[k] = origin[i];
   }
 }
 
-void MSort(vector<int> SP, vector<int> &MR, int s, int t) {
-  if (s == t) MR[s] = SP[s];
+void MSort(vector<int> origin, vector<int> &ordered, int l, int u) {
+  if (l == u) ordered[l] = origin[l];
   else {
     int m;
-    m = (s + t) / 2;
-    MSort(SP, TMR, s, m);
-    MSort(SP, TMR, m + 1, t);
-    Merge(TMR, MR, s, m, t);
+    m = (l + u) / 2;
+    MSort(origin, t_ordered, l, m);
+    MSort(origin, t_ordered, m + 1, u);
+    Merge(t_ordered, ordered, l, m, u);
   }
 }
 
@@ -37,7 +37,7 @@ int main(int argc, char * argv[]) {
   //cout << argc << endl;
   vector<int> data;
   init(data);
-  TMR.resize(data.size());
+  t_ordered.resize(data.size());
   MSort(data, data, 1, data.size() - 1);
   for (int i = 1; i < data.size(); i++) cout << data[i] << ' ';
   cout << endl;
--- dev/Library/Prim.cc
+++ dev/Library/Prim.cc
@@ -1,15 +1,83 @@
-#include <bits/stdc++.h>
+﻿#include <bits/stdc++.h>
 using namespace std;
 
-typedef pair<unsigned int, unsigned int> puu;
 
-puu loc;
+vector<vector<int> > graph;
+vector<vector<int> > gprim;
+vector<pair<int, bool> > origin;
+int N;
+pair<int, int> loc;
+const int inf = 1 << 30;
 
-map<puu, int> dist;
 
-void init() {
+void InitGraph() {
+  graph.resize(N);
+  gprim.resize(N);
+  origin.clear();
+  for (int i = 0; i < N; i++) {
+    pair<int, bool> insert;
+    insert.first = i;
+    insert.second = 0;
+    origin.push_back(insert);
+  }
+  for (int i = 0; i < N; i++) {
+    graph[i].resize(N);
+    gprim[i].resize(N);
+  }
+  int x, y, dist;
+  while(cin >> x) {
+    cin >> y >> dist;
+    graph[x][y] = graph[y][x] = dist;
+  }
+  int min = inf;
+  origin[0].second = 1;
+  for (int i = 0; i < N; i++) {
+    if (graph[0][i] != 0 && graph[0][i] < min) {
+      min = graph[0][i];
+      loc.first = 0; loc.second = i;
+    }
+  }
+  if (min != inf) {
+    graph[loc.first][loc.second] = 0; graph[loc.second][loc.first] = 0;
+    gprim[loc.first][loc.second] = min; gprim[loc.second][loc.first] = min;
+    origin[loc.second].second = 1;
+  }
+}
 
-int main(int argc, char * argv[]) {
-  init();
+
+void Prim() {
+  int size = 1;
+  while (size < origin.size()) {
+    ++size;
+    int min = inf;
+    for (int i = 0; i < N; i++) {
+      if (origin[i].second) {
+        for (int j = 0; j < N; j++) {
+          if (origin[j].second) continue;
+          if (graph[i][j] != 0 && graph[i][j] < min) {
+            min = graph[i][j];
+            loc.first = i; loc.second = j;
+          }
+        }
+      }
+    }
+    if (min != inf) {
+      graph[loc.first][loc.second] = graph[loc.second][loc.first] = 0;
+      gprim[loc.first][loc.second] = gprim[loc.second][loc.first] = min;
+      origin[loc.first].second = origin[loc.second].second = 1;
+    }
+  }
+}
+int main() {
+  //test for google interview
+  cin >> N;
+  InitGraph();
+  Prim();
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      cout << gprim[i][j] << ' ';
+    }
+    cout << endl;
+  }
   return 0;
 }
