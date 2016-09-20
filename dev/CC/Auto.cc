68680ea4e154d6d30e1de0bb14e5043d5b60d07a
yutongpa <yutong.pan@intel.com>
2016-09-20 18:45:03 +0800

    Structure Format Changed.

---
+++ dev/CC/Auto.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(int argc, char * argv[]) {
+  long long x = 1;
+  string copy(decltype(x));
+  cout << copy << endl;
+  return 0;
+}
--- dev/CC/MapUsage.cc
+++ dev/CC/MapUsage.cc
@@ -6,6 +6,9 @@ int main(int argc, char * argv[]) {
   map<double, int> test;
   test[1.1] = 4;
   test[1.2] = 1.1;
-  for (auto & it : test) cout << it.first << ' ' << it.second << endl;
+  test[2.2] = 2.1;
+  double a = 1.2;
+  test[a++]++;
+  for (auto& it : test) cout << it.first << ' ' << it.second << endl;
   return 0;
 }
